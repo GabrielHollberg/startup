9640e27a069b4b48c53091f9ddc8b9cd2e3980eb
Gabriel Hollberg <gthollberg@gmail.com>
2023-03-17 15:57:29 -0600

    Organizado

--- js/newQuote.js
+++ js/newQuote.js
@@ -2,7 +2,7 @@ function createQuote(event)
 {
     let key = event.key;
 
-    if(key == "Enter")
+    if(key === "Enter")
     {
         const inEl = document.querySelector("#quoteInput");
         localStorage.setItem("quote", inEl.value);

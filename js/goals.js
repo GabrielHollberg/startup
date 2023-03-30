a6bb47c3572492213908730aab2b932406327641
gabrielhollberg <gthollberg@gmail.com>
2023-03-30 09:36:39 -0600

    Updated goals page to create and delete goals

--- js/goals.js
+++ js/goals.js
@@ -1,4 +1,32 @@
 function createMonthly()
 {
-    alert("Feature not yet built!");
+    const myElement = document.querySelector("#weeklyGoals");
+    const myInput = document.querySelector("#goalInput");
+    const childElement = document.createElement("div");
+
+    childElement.innerText = myInput.value;
+    childElement.onclick = function() {this.remove();}
+    myElement.appendChild(childElement);
+}
+
+function createWeekly()
+{
+    const myElement = document.querySelector("#weeklyGoals");
+    const myInput = document.querySelector("#goalInput");
+    const childElement = document.createElement("div");
+
+    childElement.innerText = myInput.value;
+    childElement.onclick = function() {this.remove();}
+    myElement.appendChild(childElement);
+}
+
+function createDaily()
+{
+    const myElement = document.querySelector("#dailyGoals");
+    const myInput = document.querySelector("#goalInput");
+    const childElement = document.createElement("div");
+
+    childElement.innerText = myInput.value;
+    childElement.onclick = function() {this.remove();}
+    myElement.appendChild(childElement);
 }
